7b7ed11ac953fb12cb510b17c260b15607632bfe
David Wallace Croft <david@CroftSoft.com>
2024-10-27 12:50:55 -0500

    first system

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use bevy::prelude::*;
 
 fn main() {
-  App::new().run();
+  let mut app = App::new();
+
+  let _app: &mut App = app.add_systems(Update, my_first_system);
+
+  let _app_exit: AppExit = app.run();
+}
+
+fn my_first_system() {
+  println!("Hello, World!");
 }
