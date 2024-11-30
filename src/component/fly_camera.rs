67567566cde10404bea146202fa6b1f892f1788e
David Wallace Croft <david@CroftSoft.com>
2024-11-29 20:44:01 -0600

    FlyCameraComponent

--- src/component/fly_camera.rs
+++ src/component/fly_camera.rs
@@ -1,6 +1,7 @@
+use super::super::component::camera_settings::CameraSettingsComponent;
 use ::bevy::prelude::*;
 
 // https://www.youtube.com/watch?v=dD5-M-vUmls
 #[derive(Component)]
-#[require(CamSettings)]
+#[require(CameraSettingsComponent)]
 pub struct FlyCameraComponent;
